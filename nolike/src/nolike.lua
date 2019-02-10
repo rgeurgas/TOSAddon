c8b6fd3da088fb9beaaab5848144408aac1f2715
Crevox <crevox@gmail.com>
2019-02-10 09:33:18 -0500

    Update nolike.lua

--- nolike/src/nolike.lua
+++ nolike/src/nolike.lua
@@ -2,22 +2,6 @@ function LIKE_FAILED()
 
 end
 
-function REQUEST_LIKE_STATE(familyName)
-	local otherpcinfo = session.otherPC.GetByFamilyName(familyName);
-	
-	local frame = ui.GetFrame("compare");
-	local likeCheck = GET_CHILD_RECURSIVELY(frame,"likeCheck")
-	if session.likeit.AmILikeYou(familyName) == false then
-		if false == geClientInteraction.RequestLikeIt(otherpcinfo:GetAID(), otherpcinfo:GetCID()) then
-			likeCheck:ToggleCheck();
-		end
-	else
-		if false == geClientInteraction.RequestUnlikeIt(otherpcinfo:GetAID(), otherpcinfo:GetCID()) then
-			likeCheck:ToggleCheck();
-		end
-	end
-end
-
 function DO_CLICK_LIKECHECK()
 
 end
