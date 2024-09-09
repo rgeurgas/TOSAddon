481e1729d09a0e3accd58028f2cc71d7170d548e
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2024-09-09 20:04:57 -0300

    feat: change name

--- partymemberinfo/src/partymemberinfo.lua
+++ partymemberinfo/src/partymemberinfo.lua
@@ -1,22 +1,22 @@
-local addonName = 'partyinfo'
+local addonName = 'partymemberinfo'
 
 local acutil = require('acutil')
 local loaded = false
 
 _G['ADDONS'] = _G['ADDONS'] or {};
-local PartyInfo = _G["ADDONS"][addonName] or {};
+local PartyMemberInfo = _G["ADDONS"][addonName] or {};
 
-function PARTYINFO_GET_FILENAME()
+function PARTYMEMBERINFO_GET_FILENAME()
 	return "../wbrextend.txt";
 end
 
-function PARTYINFO_LOAD()
-	for line in io.lines(PARTYINFO_GET_FILENAME()) do
+function PARTYMEMBERINFO_LOAD()
+	for line in io.lines(PARTYMEMBERINFO_GET_FILENAME()) do
 		local teamName, gs = line:match("([^=]+)=([^=]+)");
-		local partyinfoname = PartyInfo[teamName];
+		local partyinfoname = PartyMemberInfo[teamName];
 
 		if partyinfoname == nil then
-			PartyInfo[teamName] = gs;
+			PartyMemberInfo[teamName] = gs;
 		end
 	end
 end
@@ -25,16 +25,16 @@ function MEMBERINFO_ONCLICK(frame, ctrl, argStr, argNum)
 	ui.Chat('/memberinfo ' .. argStr);
 end
 
-function PartyInfo.new(self)
+function PartyMemberInfo.new(self)
 	local members = {}
 
 	members.nameY = 0
 	members.lvboxY = 0
 
-	members.AddPartyInfo = function(self, partyInfoCtrlSet, partyMemberInfo)
+	members.AddPartyMemberInfo = function(self, partyInfoCtrlSet, partyMemberInfo)
 		local mapCls = GetClassByType("Map", partyMemberInfo:GetMapID())
 		if mapCls ~= nil then
-			local location = partyInfoCtrlSet:CreateOrGetControl('richtext', "partyinfo_location", 0, 0, 0, 0)
+			local location = partyInfoCtrlSet:CreateOrGetControl('richtext', "partymemberinfo_location", 0, 0, 0, 0)
 			location:SetText(string.format("{s12}{ol}[%s-%d]", mapCls.Name, partyMemberInfo:GetChannel() + 1))
 			location:Resize(100, 20)
 			location:SetOffset(10, 0)
@@ -43,9 +43,9 @@ function PartyInfo.new(self)
 
 		local teamName = partyMemberInfo:GetName()
 
-		if PartyInfo[teamName] ~= nil then
-			local gearscore = partyInfoCtrlSet:CreateOrGetControl('richtext', "partyinfo_gearscore", 0, 0, 0, 0)
-			gearscore:SetText(string.format("{s12}{ol}GS: %s", PartyInfo[teamName]))
+		if PartyMemberInfo[teamName] ~= nil then
+			local gearscore = partyInfoCtrlSet:CreateOrGetControl('richtext', "partymemberinfo_gearscore", 0, 0, 0, 0)
+			gearscore:SetText(string.format("{s12}{ol}GS: %s", PartyMemberInfo[teamName]))
 			gearscore:Resize(100, 20)
 			gearscore:SetOffset(10, 12)
 			gearscore:ShowWindow(1)
@@ -75,38 +75,38 @@ function PartyInfo.new(self)
 	end
 
 	members.Destroy = function(self)
-		if (PartyInfo.instance.UPDATE_PARTYINFO_HP ~= nil) then
-			UPDATE_PARTYINFO_HP = PartyInfo.instance.UPDATE_PARTYINFO_HP
+		if (PartyMemberInfo.instance.UPDATE_PARTYMEMBERINFO_HP ~= nil) then
+			UPDATE_PARTYMEMBERINFO_HP = PartyMemberInfo.instance.UPDATE_PARTYMEMBERINFO_HP
 		end
 	end
 
 	return setmetatable(members, { __index = self })
 end
 
-setmetatable(PartyInfo, { __call = PartyInfo.new });
+setmetatable(PartyMemberInfo, { __call = PartyMemberInfo.new });
 
-function PARTYINFO_ON_INIT(addon, frame)
+function PARTYMEMBERINFO_ON_INIT(addon, frame)
 	if not loaded then
-		PARTYINFO_LOAD()
+		PARTYMEMBERINFO_LOAD()
 
-		if (PartyInfo.instance.UPDATE_PARTYINFO_HP == nil) then
-			PartyInfo.instance.UPDATE_PARTYINFO_HP = UPDATE_PARTYINFO_HP
+		if (PartyMemberInfo.instance.UPDATE_PARTYMEMBERINFO_HP == nil) then
+			PartyMemberInfo.instance.UPDATE_PARTYMEMBERINFO_HP = UPDATE_PARTYMEMBERINFO_HP
 		end
 
-		UPDATE_PARTYINFO_HP = function(partyInfoCtrlSet, partyMemberInfo)
-			PartyInfo.instance.UPDATE_PARTYINFO_HP(partyInfoCtrlSet, partyMemberInfo)
+		UPDATE_PARTYMEMBERINFO_HP = function(partyInfoCtrlSet, partyMemberInfo)
+			PartyMemberInfo.instance.UPDATE_PARTYMEMBERINFO_HP(partyInfoCtrlSet, partyMemberInfo)
 			if partyMemberInfo:GetMapID() > 0 then
-				PartyInfo.instance:AddPartyInfo(partyInfoCtrlSet, partyMemberInfo)
+				PartyMemberInfo.instance:AddPartyMemberInfo(partyInfoCtrlSet, partyMemberInfo)
 			end
 		end
 
 		loaded = true
 	end
 
-	acutil.log("PartyInfo loaded!")
+	acutil.log("PartyMemberInfo loaded!")
 end
 
-if (PartyInfo.instance ~= nil) then
-	PartyInfo.instance:Destroy()
+if (PartyMemberInfo.instance ~= nil) then
+	PartyMemberInfo.instance:Destroy()
 end
-PartyInfo.instance = PartyInfo()
+PartyMemberInfo.instance = PartyMemberInfo()
